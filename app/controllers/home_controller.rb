680949600c5b7ae4714b00f1d546f98c856b41fc
Harry Wijnschenk <harry.wijnschenk@thecurve.io>
2024-08-10 17:26:35 +0100

    Widgets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @sale_count = Sale.count
+    @visitor_count = Visitor.count
+  end
+
+  def widget_stats
+    render json: { sale_count: Sale.count, visitor_count: Visitor.count }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  get "widget-stats" => "home#widget_stats"
 end
